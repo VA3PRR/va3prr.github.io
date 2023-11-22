9ebc398587525bc7256e221dfe45d724cff425af
J <96140053+VA3PRR@users.noreply.github.com>
2023-11-22 04:33:28 -0500

    Updated style and js
    Added navmenu button

--- include/main.js
+++ include/main.js
@@ -1,5 +1,6 @@
 document.onLoad = getSource("navigation", "navigation");
 document.onLoad = getSource("main", "content");
+nav_menu_visible = "none";
 function getSource(href, target)	{
 	var xmlhttp	=	new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function()	{
@@ -7,7 +8,7 @@ function getSource(href, target)	{
 			document.getElementById(target).innerHTML = xmlhttp.responseText;
 		}
 	}
-	href = "content/"+href+".html";
+	href = "/content/"+href+".html";
 	xmlhttp.open("GET", href, true);
 	xmlhttp.send();
 }
@@ -19,11 +20,23 @@ function getPage(href)	{
 			document.getElementById("content").innerHTML = xmlhttp.responseText;
 		}
 	}
-	href = "content/"+href+".html";
+	href = "/content/"+href+".html";
 	xmlhttp.open("GET", href, true);
 	xmlhttp.send();
 }
 
 function loadImage(href)	{
 	document.getElementById("main_image").src = href;
+}
+
+function toggleMenu(caller)	{
+	div = document.getElementById("nav_menu");
+	if(nav_menu_visible == "none")	{
+		nav_menu_visible = "inline-flex";
+	}
+	else	{
+		nav_menu_visible = "none";
+	}
+	div.style.display = nav_menu_visible;
+	console.log(div.style.display);
 }
